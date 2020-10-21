9b3f0a3368fd389a62cca04532d726720c9fd157
niksaz <nikitasazanovich@gmail.com>
2020-10-21 16:27:20 +0300

    Dataset iteration test.

---
+++ train_unit_test.py
@@ -0,0 +1,30 @@
+# Author: Mikita Sazanovich
+
+import os
+import unittest
+import time
+
+import configuration
+import train_unit
+
+
+def print_time(start, ended):
+  print(f'Time spent: {ended - start:.4f}s')
+
+
+class TestDataset(unittest.TestCase):
+  def test_iteration(self):
+    config_path = os.path.join('exps', 'unit', 'duckietown_unit.yaml')
+    config = configuration.load_config(config_path)
+    a_train_dataset, a_test_dataset, a_test_length = train_unit.create_image_action_dataset(config, 'domain_a')
+    b_train_dataset, b_test_dataset, b_test_length = train_unit.create_image_action_dataset(config, 'domain_b')
+
+    for dataset in [a_train_dataset, a_test_dataset, b_train_dataset, b_test_dataset]:
+      time_start = time.time()
+      for batch_tuple in dataset:
+        print(f'Dataset has been tested. The length of the batch tuple is {len(batch_tuple)}.')
+        for el in batch_tuple:
+          print(el.shape)
+        break
+      time_ended = time.time()
+      print_time(time_start, time_ended)
