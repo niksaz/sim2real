5df015b90a94709bd51c707ef7a9fd14e87d2d90
niksaz <nikitasazanovich@gmail.com>
2021-01-13 23:03:41 +0300

    Script for reproducing the initial experiments.

--- run_experiments.py
+++ run_experiments.py
@@ -1,5 +1,6 @@
 # Author: Mikita Sazanovich
 
+import itertools
 import os
 import configuration
 
@@ -21,20 +22,47 @@ def run_experiment(config, update_params, tag):
   os.system(f'python train_unit.py {tag} --config_path {generated_config_path}')
 
 
+def iterations_to_desc(iterations: int) -> str:
+  return f'{iterations // 1000}K'
+
+
 def main():
   original_config_path = 'configs/unit/duckietown_unit.yaml'
   config = configuration.load_config(original_config_path)
-  iterations = 10000
-  for repeat in range(3):
-    for tcc_w in [0.0, 0.1, 1.0, 10.0, 100.0]:
-      tag = 'TCC'
-      tag += f'-W-{tcc_w}'
-      run_experiment(
-          config, {
-              'hyperparameters/iterations': iterations,
-              'hyperparameters/loss/tcc_w': tcc_w,
-          },
-          tag)
+  iterations = 25000
+  tag_to_update_params = {
+      f'SIM2REAL-SHUFFLE-{iterations_to_desc(iterations)}': {
+          'hyperparameters/shuffle_episode_indexes': True,
+          'hyperparameters/loss/triplet_w': 0.0,
+          'hyperparameters/loss/triplet_margin': 0.0,
+          'hyperparameters/loss/tcc_w': 0.0,
+          'hyperparameters/iterations': iterations,
+      },
+      f'SIM2REAL-TRIPLET-0.1-MAR-1.0-SHUFFLE-{iterations_to_desc(iterations)}': {
+          'hyperparameters/shuffle_episode_indexes': True,
+          'hyperparameters/loss/triplet_w': 0.1,
+          'hyperparameters/loss/triplet_margin': 1.0,
+          'hyperparameters/loss/tcc_w': 0.0,
+          'hyperparameters/iterations': iterations,
+      },
+      f'SIM2REAL-TCC-0.1-SHUFFLE-{iterations_to_desc(iterations)}': {
+          'hyperparameters/shuffle_episode_indexes': True,
+          'hyperparameters/loss/triplet_w': 0.0,
+          'hyperparameters/loss/triplet_margin': 0.0,
+          'hyperparameters/loss/tcc_w': 0.1,
+          'hyperparameters/iterations': iterations,
+      },
+      f'SIM2REAL-TCC-0.1-TRIPLET-0.1-MAR-1.0-SHUFFLE-{iterations_to_desc(iterations)}': {
+          'hyperparameters/shuffle_episode_indexes': True,
+          'hyperparameters/loss/triplet_w': 0.1,
+          'hyperparameters/loss/triplet_margin': 1.0,
+          'hyperparameters/loss/tcc_w': 0.1,
+          'hyperparameters/iterations': iterations,
+      },
+  }
+  for _ in itertools.count():
+    for tag, update_params in tag_to_update_params.items():
+      run_experiment(config, update_params, tag)
 
 
 if __name__ == '__main__':
